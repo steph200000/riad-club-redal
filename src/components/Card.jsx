0867f4bee256c518f91cf2daa3cb5e1ae917911a
steph200000 <mustaphaderbal9@gmail.com>
2026-02-19 15:15:31 +0000

    update v15

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -46,7 +46,7 @@ const Card = ({ title, desc, bullets, img, icon, onMore }) => {
                   e.stopPropagation();
                   onMore();
                 }}
-                className={`rounded-full bg-emerald-700 px-4 py-2 text-xs font-semibold text-white shadow-xl hover:bg-emerald-800 flex-shrink-0 whitespace-nowrap sm:opacity-0 sm:group-hover:opacity-100 sm:translate-y-0 sm:transition-opacity sm:duration-300 self-end sm:self-auto ${
+                className={`rounded-full bg-emerald-700 px-4 py-2 text-xs font-semibold text-white shadow-xl hover:bg-emerald-800 flex-shrink-0 whitespace-nowrap sm:opacity-0 sm:group-hover:opacity-100 sm:group-hover:-translate-y-1 sm:transition-all sm:duration-300 self-end sm:self-auto ${
                   isMobileClicked 
                     ? 'opacity-100 translate-y-0 -translate-y-1 scale-100' 
                     : 'opacity-0 translate-y-2 scale-95'
